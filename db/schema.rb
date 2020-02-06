f28c3fbce30bf19970718ac5efcbf77b617364ec
Rachid Khalidi rkhal@yahoo.fr <rkhal@yahoo.fr>
2020-02-06 20:24:02 +0000

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_202940) do
+ActiveRecord::Schema.define(version: 2020_02_06_200333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 2020_02_04_202940) do
     t.integer "servings"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "meal_id"
+    t.index ["meal_id"], name: "index_bookings_on_meal_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "meals", force: :cascade do |t|
@@ -48,5 +52,7 @@ ActiveRecord::Schema.define(version: 2020_02_04_202940) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "meals"
+  add_foreign_key "bookings", "users"
   add_foreign_key "meals", "users"
 end
